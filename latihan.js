b2c47b1369f6047c316d66eedea9610799586781
salafiyah523 <muhimsalafiyah@gmail.com>
2025-01-08 11:17:38 +0700

    Add files via upload

---
+++ latihan.js
@@ -0,0 +1,20 @@
+let nama = "Muhimmatus Salafiyah";
+let alamat = "Dempet Tugu RT02/RW03";
+let usia = "15";
+let ttl = "Demak,20 April 2009";
+let gender = "Perempuan";
+let agama = "Islam";
+let email = "muhimsalafiyah@gmail.com";
+let berat = "49kg";
+let tinggi = "149cm";
+let hobi = "membaca";
+let cita = "Jadi Orang Sukses";
+let status = "pelajar";
+let sekolah = "SMK NEGERI 1 SAYUNG";
+let jurusan = "PPLG";
+document.write(" nama saya adalah " + nama + " saya tinggal di " + alamat +
+    " pada tahun 2025 ini usia saya adalah " + usia + " saya lahir di" + ttl +
+    " jenis kelamin saya adalah " + gender + " saya beragama " + agama + " alamat email saya " + 
+    email + " saya memiliki berat badan " + berat + " dengan tinggi " + tinggi + " saya sangat senang " +
+    hobi + " karena saya memiliki cita cita " + cita + " saat ini saya adalah seorang " + student +
+    " yang bersekolah di " + sekolah + " pada jurusan " + jurusan )
